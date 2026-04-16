5f66c59be3a366ec131076e009130a938940c357
LaiVanChung <126126468+tothichcau02@users.noreply.github.com>
2026-04-16 22:25:12 +0700

    dsa flow 13

--- 13.js
+++ 13.js
@@ -25,3 +25,22 @@ var romanToInt = function(s) {
     }
     return total;
 };
+LVIII
+`i = 0, current = 50, next = 5 => total = 50
+i = 1, current = 5, next = 1 => total = 55
+i = 2, current = 1, next = 1 => total = 56
+i = 3, current = 1, next = 1 => total = 57
+i = 4, current = 1, next = undefined => total = 58`
+
+MCMXCIV
+`
+i = 0, current = 1000, next = 100 => total = 1000
+i = 1, current = 100, next = 1000 => total = 900
+i = 2, current = 1000, next = 10 => total = 1900
+i = 3, current = 10, next = 100 => total = 1890
+i = 4, current = 100, next = 1 => total = 1990
+i = 5, current = 1, next = 5 => total = 1989
+i = 6, current = 5, next = undefined => total = 1994
+`
+
+
